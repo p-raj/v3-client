24523e0c60ba31482a80e0af0a0a68b76daaa91a
Abhinav Anand <abhinav.anand@veris.in>
2017-01-18 16:39:09 +0530

    feat: add routing for native App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,17 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {Provider} from "react-redux";
+import store from "./store";
+import AuthScreen from "./screens/AuthScreen";
+import { nativeHistory, Route, Router,} from 'react-router-native';
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
-            </View>
+            <Provider store={store}>
+                <Router history={nativeHistory}>
+                    <Route path="/" component={AuthScreen}/>
+                </Router>
+            </Provider>
         )
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
