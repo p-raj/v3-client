8be147af72a3f7b062b68b89342c4e391eff4efa
Abhinav Anand <abhinav.anand@veris.in>
2017-01-18 18:59:47 +0530

    fix: add routerReducer to root reducer

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import auth from "./auth";
+import {routerReducer} from "react-router-redux";
 import {idbReducer} from "./idbReducer";
 
 
 let reducer = combineReducers({
     idb: idbReducer,
+    routing: routerReducer,
     auth: auth
 });
 
