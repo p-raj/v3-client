a90c58482563b44b822f293f7d8555c86bd48c67
Abhinav Anand <abhinav.anand@veris.in>
2017-01-18 18:59:16 +0530

    feat: add action & reducer to load data from IDB

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import auth from "./auth";
+import {idbReducer} from "./idbReducer";
 
 
 let reducer = combineReducers({
+    idb: idbReducer,
     auth: auth
 });
 
