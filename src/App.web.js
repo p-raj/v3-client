83fcca6e9614649b0d0db23312dd9e35952cf181
Abhinav Anand <abhinav.anand@veris.in>
2017-01-19 16:52:49 +0530

    feat: add SplashScreen to App.web.js

--- src/App.web.js
+++ src/App.web.js
@@ -3,7 +3,9 @@ import {Provider} from "react-redux";
 import {Router, Route, browserHistory} from "react-router";
 import store from "./store";
 import AuthScreen from "./screens/AuthScreen";
+import SplashScreen from "./screens/SplashScreen";
 import {syncHistoryWithStore} from "react-router-redux";
+
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
@@ -14,6 +16,7 @@ class App extends React.Component {
         return (
             <Provider store={store}>
                 <Router history={history}>
+                    <Route path="/" component={SplashScreen}/>
                 </Router>
             </Provider>
         );
