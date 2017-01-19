9c312742aac83af42fa706ecf5c102a1eb95fb5f
Abhinav Anand <abhinav.anand@veris.in>
2017-01-19 16:25:04 +0530

    feat: add action & reducer for refreshToken

--- src/common/endpoints.js
+++ src/common/endpoints.js
@@ -1,1 +1,2 @@
 export const LOGIN = "/api/auth/login/";
+export const REFRESH_TOKEN = "/oauth/token/";
--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,13 +1,15 @@
 import {combineReducers} from "redux";
-import auth from "./auth";
+import authReducer from "./authReducer";
 import {routerReducer} from "react-router-redux";
 import {idbReducer} from "./idbReducer";
+import refreshTokenReducer from "./refreshTokenReducer";
 
 
 let reducer = combineReducers({
     idb: idbReducer,
     routing: routerReducer,
-    auth: auth
+    auth: authReducer,
+    refreshToken: refreshTokenReducer,
 });
 
 export default reducer;
