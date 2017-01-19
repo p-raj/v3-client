dacdcebdfd0a9d967e99073e05d8ec42d4619cdd
Abhinav Anand <abhinav.anand@veris.in>
2017-01-19 16:29:36 +0530

    feat: use react-router-redux for history

--- src/App.web.js
+++ src/App.web.js
@@ -3,14 +3,17 @@ import {Provider} from "react-redux";
 import {Router, Route, browserHistory} from "react-router";
 import store from "./store";
 import AuthScreen from "./screens/AuthScreen";
+import {syncHistoryWithStore} from "react-router-redux";
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store);
 
 class App extends React.Component {
 
     render() {
         return (
             <Provider store={store}>
-                <Router history={browserHistory}>
-                    <Route path="/" component={AuthScreen}/>
+                <Router history={history}>
                 </Router>
             </Provider>
         );
