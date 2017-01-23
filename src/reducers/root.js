46e21a9ff5c8346d3575b4eb8b8be062d88f4f23
Abhinav Anand <abhinav.anand@veris.in>
2017-01-23 11:24:11 +0530

    feat: add action & reducer to get runtimes

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -4,6 +4,7 @@ import {routerReducer} from "react-router-redux";
 import {idbReducer} from "./idbReducer";
 import refreshTokenReducer from "./refreshTokenReducer";
 import membershipsReducer from "./membershipsReducer";
+import runtimesReducer from "./runtimesReducer";
 
 
 let reducer = combineReducers({
@@ -12,6 +13,7 @@ let reducer = combineReducers({
     auth: authReducer,
     refreshToken: refreshTokenReducer,
     memberships: membershipsReducer,
+    runtimes: runtimesReducer,
 });
 
 export default reducer;
