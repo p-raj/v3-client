b353b7203b3fac3740c9a2f91a91db2a4e801664
Abhinav Anand <abhinav.anand@veris.in>
2017-01-23 11:25:24 +0530

    feat: bind MembershipsScreen with runtimes

--- src/screens/MembershipsScreen.js
+++ src/screens/MembershipsScreen.js
@@ -4,12 +4,16 @@ import {withRouter} from "react-router";
 import getMemberships from "../actions/getMemberships";
 import {FAILED, START} from "../common/constants";
 import _ from "lodash";
+import Thumbnail from "../components/Thumbnail";
+import Runtime from "../components/Runtime";
+
 
 class MembershipsScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            memberships: []
+            memberships: [],
+            selectedIndex: 0
         }
     }
 
@@ -18,7 +22,6 @@ class MembershipsScreen extends React.Component {
         if (nextProps.memberships.status === FAILED && nextProps.memberships.statusCode === 401) {
             // Token has expired but not by timeout so move to login screen
             this.props.router.replace('/auth');
-            return;
         }
     }
 
@@ -29,20 +32,49 @@ class MembershipsScreen extends React.Component {
             )
         }
 
-        if (!_.isEmpty(this.props.memberships.results)) {
-            return (
-                <p>{this.props.memberships.results[0].url}</p>
-            )
-        }
-        else {
+        if (_.isEmpty(this.props.memberships.results)) {
             return (
                 <p>No membership data available</p>
             )
         }
+
+        // Memberships results is not empty
+        // FIXME: render is called multiple times. We should not create variables here.
+        let thumbs = [];
+        let index = 0;
+        for (let item of this.props.memberships.results) {
+
+            thumbs.push(<Thumbnail url={item.organization.logo}
+                                   key={item.id}
+                                   index={index}
+                                   onClick={this.onThumbClicked.bind(this,index)}/>
+            );
+
+            index++;
+        }
+
+        return (
+            <div>
+                <div>
+                    {thumbs}
+                </div>
+
+                <div>
+                    <Runtime data={this.props.memberships.results[this.state.selectedIndex]}
+                             key={this.props.memberships.results[this.state.selectedIndex].id}/>
+                </div>
+            </div>
+        )
     }
 
     componentDidMount() {
         this.props.dispatch(getMemberships(this.props.auth.access_token));
+    }
+
+    onThumbClicked(index) {
+        this.setState({
+            selectedIndex: index
+        })
     }
 }
 
